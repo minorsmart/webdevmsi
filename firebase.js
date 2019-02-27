e4035b5d03fe6a645b126c59b355ebd15c1178e7
witusj <wfth69@gmail.com>
2019-02-27 23:16:25 +0100

    Update

--- firebase.js
+++ firebase.js
@@ -11,8 +11,11 @@ firebase.initializeApp(config);
 
 var db = firebase.firestore();
 
-var button = document.getElementById("submitMessage");
+// Select buttons
+var postbtn = document.getElementById("submitMessage");
+var getbtn = document.getElementById("getData");
 
+// Function for writing to DB
 writeDB = (val1, val2) => {
     console.log("Button clicked and received", val1, val2)
     db.collection("users").add({
@@ -27,9 +30,36 @@ writeDB = (val1, val2) => {
     });
 }
 
-button.onclick = () => {
+// Function for reading from DB
+readDB = () => {
+    console.log("Data button pushed")
+    db.collection("users")
+    .get()
+    .then( (querySnapshot) => {
+        querySnapshot.forEach( (doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+}
+
+// Prevent page scroll after form submit
+postbtn.addEventListener("click", function(event){
+    event.preventDefault()
+  });
+getbtn.addEventListener("click", function(event){
+    event.preventDefault()
+  });
+
+// Send and retrieve data on button clicks
+postbtn.onclick = () => {
     var address = document.getElementById("inputEmail").value
     var body = document.getElementById("textMessage").value
     writeDB(address, body)
     document.getElementById("contactForm").reset();
 }
+
+getbtn.onclick = () => readDB()
